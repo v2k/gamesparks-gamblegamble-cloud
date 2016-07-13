43eff2a3a5d76ab86f9703257e4d8a817b2a689a
v2k <vince.jewlal@dena.com>
2016-07-13 00:12:55 -0700

    handle event request

---
+++ request/LogChallengeEventRequest.js
@@ -0,0 +1,8 @@
+// ====================================================================================================
+//
+// Cloud Code for LogChallengeEventRequest, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+// here?
