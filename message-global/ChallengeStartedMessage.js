a51ba4484c19bd8e36e90b68e64425a6137ce25b
v2k <vince.jewlal@dena.com>
2016-07-12 20:25:01 -0700

    test no consume

--- message-global/ChallengeStartedMessage.js
+++ message-global/ChallengeStartedMessage.js
@@ -4,31 +4,6 @@
 // pick the dealer/first to start
 // score, stats => 0
 // pass state to starting player -> OnRoundStarted
-
-//Loads the cards module with the functions we need
-//gs_load("dealer");
-
-//Gets the current challenge so we can populate it with cards and hands
-//This uses the GameSparks "Spark" API (Documented in the portal)
-//var chal = Spark.getChallenge(Spark.data.challenge.challengeId);
-
-//Create an empty array to hold the hand state
-//var stats = [];   
-
-//Gets the list of player ID's who have accepted the challenge
-//var accepted = challenge.getAcceptedPlayerIds();
-
-//Create an empty hand object for each player and add it to the hands array
-//for(i=0 ; i<accepted.length ; i++){
-//hands[i] = {id:accepted[i], hand:[]};
-//}
-
-//var playerStats = chal.getScriptData("playerStats");
-//Store the initialised hands array against the challenge
-//challenge.setScriptData("visible_hands", hands);
-//challenge.setScriptData("playerStats", hands);
-
-//Declare challenge
 var challenge = Spark.getChallenge(Spark.getData().challenge.challengeId);
 var spark = Spark.getData();
 var sparkChal = Spark.getData().challenge;
@@ -49,7 +24,6 @@ for (i = 0; i < players.length; i++) {
 	playerStats[players[i]] = {"score": 0, "numFL": 0, "cardsPulled": 0, "inFantasyland": false, "hasPulled": false }
 }
 
-
 // use the given next player, shuffle the rest?
 var playerOrder = [];
 playerOrder.push(firstPlayer);
@@ -63,47 +37,10 @@ for (i = 0; i < players.length; i++) {
 
 dealer = playerOrder[playerOrder.length - 1];
 
-
-/*
-while (dealer == null)
-{
-	lastPlayer = nextPlayer;
-	var ret = challenge.consumeTurn(nextPlayer);
-	sparkChal = Spark.getData().challenge;
-	nextPlayer = sparkChal.nextPlayer;
-	
-	if (nextPlayer == firstPlayer)
-	{
-		dealer = lastPlayer;
-		break;
-	}
-}
-*/
-
-//Construct the play field JSON - Used for the playing field
-//var board = {};
-//board[challengerId] = {};
-//board[challengedId] = {};
-
-//Construct the current hand JSON - Used for the cards in the player's hands
-//var currentHand = {};
-//currentHand[challengerId] = {};
-//currentHand[challengedId] = {};
-
-//var playersData = {};
-//currentHand[challengedId] = drawn;
-
-//Construct player details
-//playerStats[challengerId] = {"score": 0, "numFL": 0, "cardsPulled": 0, "hasPulled": false }
-//playerStats[challengedId] = {"score": 0, "numFL": 0, "cardsPulled": 0, "hasPulled": false }
-
 // handled by gamesparks for turn start
 var gameState = { actionIndex:0, "first":firstPlayer, "dealer":dealer, "turn":0, "round":0, "num_players": players.length, "is_last_move": false };
 
 //Save the contructed JSONs against the challenge's scriptData
-//chal.setScriptData("deck", deck);
-//chal.setScriptData("board", board);
-//chal.setScriptData("currentHand", currentHand);
 challenge.setScriptData("playerStats", playerStats);
 challenge.setScriptData("gameState", gameState);
 challenge.setScriptData("playerOrder", playerOrder);
--- message-global/ChallengeTurnTakenMessage.js
+++ message-global/ChallengeTurnTakenMessage.js
@@ -1,7 +1,6 @@
 // GLOBAL MESSAGE
 
 // if we're not on the correct player, consume turn and move to next player
-
 var challenge = Spark.getChallenge(Spark.getData().challenge.challengeId);
 var sparkChal = Spark.getData().challenge;
 var nextPlayer = sparkChal.nextPlayer;
@@ -10,7 +9,7 @@ var nextPlayer = sparkChal.nextPlayer;
 var gameState = challenge.getScriptData("gameState"); 
 var playerOrder = challenge.getScriptData("playerOrder");
 
-var isFinalMove = challenge.getScriptData("isFinalMove");
+//var isFinalMove = challenge.getScriptData("isFinalMove");
 
 if (nextPlayer == playerOrder[gameState.actionIndex])
 {
@@ -19,5 +18,5 @@ if (nextPlayer == playerOrder[gameState.actionIndex])
 }
 else
 {
-	challenge.consumeTurn(nextPlayer);
+	//challenge.consumeTurn(nextPlayer);
 }
